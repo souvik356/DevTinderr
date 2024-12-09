8d5c6a7bbf6f00d928597017759769827503cdb1
Souvik Roy <souvik_roy@thbs.india.com>
2024-12-09 17:36:40 +0530

    add dependency for nodemon

--- src/App.js
+++ src/App.js
@@ -6,6 +6,6 @@ app.use('/test',(req,res)=>{
    res.send("Hello from the server")
 })
 
-app.listen(3000,()=>{
-    console.log("Server is succesfully listening on port 3000");
+app.listen(7777,()=>{
+    console.log("Server is succesfully listening on port 7777");
 })
